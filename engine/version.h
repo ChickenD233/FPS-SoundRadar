81bd1c16ddb4989eadba91938f310fc11f85e772
ChickenD233 <dl18710255985@outlook.com>
2026-09-21 10:03:51 +0800

    v2.1.2: drop the mouse view-turn experiment

--- engine/version.h
+++ engine/version.h
@@ -1,4 +1,4 @@
 #pragma once
 // single source of truth for the app version (GUI title, release tags)
-#define SR_APP_VERSION L"2.1.1"
-#define SR_APP_VERSION_A "2.1.1"
+#define SR_APP_VERSION L"2.1.2"
+#define SR_APP_VERSION_A "2.1.2"
